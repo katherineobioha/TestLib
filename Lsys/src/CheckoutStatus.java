7141795ae2dcf1a0adb9fb2e4723e3865a1f2168
katherineobioha <katobioha@gmail.com>
2017-04-22 15:42:06 -0400

    Adding Library system

---
+++ Lsys/src/CheckoutStatus.java
@@ -0,0 +1,8 @@
+
+public enum CheckoutStatus {
+	SUCCESS
+// is success
+	//is checkedout
+	//not in lib
+, IS_CHECKEDOUT, NOT_IN_LIBRARY
+}
---
+++ Lsys/src/ReturnStatus.java
@@ -0,0 +1,9 @@
+
+public enum ReturnStatus {
+	SUCCESS, NOT_CHECKEDOUT, NO_SUCH_USER
+	
+	// succesful
+	//not checked out
+	//no such user
+
+}
