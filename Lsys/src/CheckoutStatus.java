49751d1f5052353afb61b76cc2c4dbde9df08954
katherineobioha <katobioha@gmail.com>
2017-04-22 21:18:27 -0400

    Remove Lsys

--- Lsys/src/CheckoutStatus.java
+++
@@ -1,8 +0,0 @@
-
-public enum CheckoutStatus {
-	SUCCESS
-// is success
-	//is checkedout
-	//not in lib
-, IS_CHECKEDOUT, NOT_IN_LIBRARY
-}
--- Lsys/src/ReturnStatus.java
+++
@@ -1,9 +0,0 @@
-
-public enum ReturnStatus {
-	SUCCESS, NOT_CHECKEDOUT, NO_SUCH_USER
-	
-	// succesful
-	//not checked out
-	//no such user
-
-}
